3dec9c107b9516ef5def1647071b2e806b93f92d
hkim113005 <hyungjae1130@gmail.com>
2021-07-16 15:19:33 +0900

    changed to odd numbers

--- thermal_detection/pyText.py
+++ thermal_detection/pyText.py
@@ -1,11 +1,3 @@
 for number in range(1, 11):
-<<<<<<< HEAD
-<<<<<<< HEAD
-	if number % 2 == 0:
-=======
-	if number % 2 == 1:
->>>>>>> Jae
-=======
-	if number % 2 == 1:
->>>>>>> 0c0132aaf3679f665c1073048ca2a7d0f3c60494
+	if number % 2 == 1;
 		print(n);
